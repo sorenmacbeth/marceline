7a4f750e08bde900ff8092febd6c9dc13830fd4d
Homer Strong <homer.strong@gmail.com>
2013-10-25 14:51:49 -0700

    fix feeder-spout and rename global test variable

--- test/marceline/storm/testing/trident_wordcount_test.clj
+++ test/marceline/storm/testing/trident_wordcount_test.clj
@@ -10,7 +10,7 @@
         storm.trident.testing
         marceline.storm.testing.trident-wordcount))
 
-(def test-vals [["the cow jumped over the moon"]
+(def TEST-VALS [["the cow jumped over the moon"]
                 ["four score and seven years ago"]
                 ["how many can you eat"]
                 ["to be or not to be the person"]])
@@ -18,10 +18,10 @@
 (deftest wordcount-drpc
   (t/with-local-cluster [cluster]
     (with-drpc [drpc]
-      (let [feeder (t/feeder-spout ["sentence"])
+      (let [feeder (feeder-spout ["sentence"])
             topology (build-topology feeder drpc)]
         (with-topology [cluster topology]
-          (feed feeder test-vals)
+          (feed feeder TEST-VALS)
           (is (= 4
                  (ffirst
                   (exec-drpc drpc
