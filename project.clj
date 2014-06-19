17c1ac01e89414a7a9553a0a83c8e5f74eea9e43
Homer Strong <homer.strong@gmail.com>
2014-06-18 18:38:03 -0700

    version rc1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yieldbot/marceline "0.2.0-SNAPSHOT"
+(defproject yieldbot/marceline "0.2.0-rc1"
   :description "A Clojure DSL for Storm/Trident."
   :url "https://github.com/yieldbot/marceline"
   :license {:name "Eclipse Public License"
